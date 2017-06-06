3e1d37b8c2c1204dfa3182608aa0aa18beae4198
Chris Draycott-Wheatley <chriswheatley87@gmail.com>
2017-06-06 08:09:26 +0100

    move styles out of document

--- components/navigation.js
+++ components/navigation.js
@@ -46,6 +46,87 @@ export default ({ current }) => {
       >
         <GitHubRibbon />
       </a>
+      <style global jsx>
+        {
+          `
+        @import 'tachyons/css/tachyons.min.pure';
+      `
+        }
+      </style>
+      <style global jsx>
+        {
+          `
+        body {
+          overflow-y: scroll;
+          max-width: 100%;
+          overflow-x: hidden;
+        }
+
+        .page-transition-appear {
+          transition: opacity .3s ease-in-out;
+          opacity: 0;
+        }
+        .page-transition-appear.page-transition-appear-active {
+          opacity: 1;
+        }
+        .page-transition-leave {
+          transition: opacity .3s ease-in-out;
+          opacity: 1;
+        }
+        .page-transition-leave.page-transition-leave-active {
+          opacity: 0;
+        }
+
+        .github-corner {
+          position: absolute;
+          top: 0;
+          right: 0;
+          height: 5em;
+          width: 5em
+        }
+
+        .github-corner:hover .octo-arm, .github-corner:focus .octo-arm {
+          animation: octocat-wave 560ms ease-in-out
+        }
+
+        @keyframes octocat-wave {
+          0%, 100% {
+            transform: rotate(0)
+          }
+          20%, 60% {
+            transform:rotate(-25deg)
+          }
+          40%, 80% {
+            transform:rotate(10deg)
+          }
+        }
+
+        #nprogress {
+          pointer-events: none;
+        }
+
+        #nprogress .bar {
+          background: #ff80cc;
+          position: fixed;
+          z-index: 1031;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 2px;
+        }
+        #nprogress .peg {
+          display: block;
+          position: absolute;
+          right: 0px;
+          width: 100px;
+          height: 100%;
+          box-shadow: 0 0 10px #ff80cc, 0 0 5px #ff80cc;
+          opacity: 1.0;
+          transform: rotate(3deg) translate(0px, -4px);
+        }
+        `
+        }
+      </style>
     </section>
   );
 };
